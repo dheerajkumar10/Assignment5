11c7aaf2d18f86c5236f9ab6f442105a22387042
Dheeraj <dheerajkumart619@gmail.com>
2022-06-30 18:38:01 -0500

    changes1

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
+from cgi import test
 from flask import Flask, render_template, request
 import os.path
 import nltk
 import re
 from operator import itemgetter
+from nltk.tokenize import word_tokenize
 
 
 app = Flask(__name__)
@@ -26,35 +28,34 @@ def search():
 
 testdict = {}
 list = []
+with open('./SpanishStopWords.txt', 'r') as f1:
+    stopWords = f1.readlines()
+    stopWords = [i.strip() for i in stopWords]
 
 
 @app.route('/filter1', methods=['POST', 'GET'])
 def filter1():
-    number = int(request.form["number"])
-    f = open("./Grimm.txt", "r", errors='ignore')
-    data_file = f.read()
-    sentences = nltk.sent_tokenize(data_file)
-    for i in range(len(sentences)):
-        sentences[i] = sentences[i].split(' ', 1)[1]
-        sentences[i] = re.sub(r'[^\w\s]+', ' ', sentences[i])
-        tokens = nltk.word_tokenize(sentences[i])
-        for word in tokens:
-            if word.isupper():
-                if word not in testdict:
-                    testdict[word] = 1
-                else:
-                    testdict[word] = testdict[word]+1
-    res = dict(sorted(testdict.items(),
-               key=itemgetter(1), reverse=True)[:number])
-    return render_template('1.html', data=res, len=len(res))
+    with open('./Alamo.txt', 'r', encoding="utf8") as file:
+        sentences = file.readlines()
+        for i in range(len(sentences)):
+            tokens = nltk.word_tokenize(sentences[i])
+            for word in tokens:
+                if word in stopWords:
+                    if word not in testdict:
+                        testdict[word] = 1
+                    else:
+                        testdict[word] = testdict[word]+1
+    return render_template('1.html', data=testdict)
 
 
 @app.route('/filter2', methods=['POST', 'GET'])
 def filter2():
     rtxt = request.form['rtxt']
     rtxt1 = request.form['rtxt1']
-    with open('Grimm.txt', 'r') as file:
-        filedata = file.read()
+    f = open("Akamo.txt", "r")
+    data_file = f.read()
+
+    sentences = nltk.sent_tokenize(data_file)
 
     filedata = filedata.replace(rtxt, rtxt1)
     with open('Grimm.txt', 'w') as file:
